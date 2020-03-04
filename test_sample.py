2106ecb45e5e884c71d48549332a07ee4d05251c
kristofer <kristofergauti@gmail.com>
2020-03-04 13:01:37 +0000

    test3 failed

--- test_sample.py
+++ test_sample.py
@@ -1,5 +1,14 @@
 import sample
 
 def test1():
-    assert sample.fizzbuzz(15) == "FizzBuzz"
+    assert sample.fizzbuzz(2) == 2
+
+def test2():
+    assert sample.fizzbuzz(5) == "Buzz"
+
+def test3():
+    assert sample.fizzbuzz(3) == "Fizz"
+
+
+
 
